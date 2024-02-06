034d1ecab2025dcf1158082916bfe1006387a4e7
majafranz <majafranz99@gmail.com>
2024-02-06 10:44:12 +0100

    Calculate Fourier coefficients for VQE

--- vqe/config.py
+++ vqe/config.py
@@ -1,7 +1,7 @@
 global_seed = 777
 
 # Problem settings
-n_qubits = 4 # Number of vertices in the MAXCUT problem graph
+n_qubits = 2 # Number of vertices in the MAXCUT problem graph
 edge_prob = 0.7 # Edge probability for Erdos-Renyi graph
 problem_seed = 777
 
--- vqe/main.py
+++ vqe/main.py
@@ -1,6 +1,11 @@
 import pennylane as qml
 import tensorflow as tf
 from keras.optimizers import SGD
+import numpy as np
+from functools import partial
+from pennylane.fourier import coefficients
+from pennylane.fourier.visualize import bar
+import matplotlib.pyplot as plt
 
 # ignore tensorflow's complex->float cast warning
 tf.get_logger().setLevel('ERROR')
@@ -17,11 +22,11 @@ def optimise_VQE():
     """
     m_ising, offset = qu.provide_random_maxcut_ising(cfg.n_qubits, cfg.edge_prob, cfg.problem_seed)
 
-    parameters = tf.Variable(tf.random.uniform([cfg.n_layers, 3, cfg.n_qubits]),
+    parameters = tf.Variable(tf.random.uniform((cfg.n_layers * 2 * cfg.n_qubits,)),
                              trainable=True)
 
     dev = qml.device("default.qubit")
-    circ = qml.QNode(cu.circ_19, dev, interface="tf")
+    circ = qml.QNode(cu.circ_2, dev, interface="tf")
 
     observable = cu.ising_hamiltonian(m_ising)
 
@@ -32,7 +37,7 @@ def optimise_VQE():
     for i in range(cfg.max_iterations):
         with tf.GradientTape() as tape:
 
-            cost = circ(parameters, observable) + offset
+            cost = circ(parameters, observable, cfg.n_layers) + offset
 
         grads = tape.gradient(cost, parameters)
 
@@ -44,9 +49,26 @@ def optimise_VQE():
             break
 
     print(f"Reached a minimum cost of {costs[-1].numpy():.4f} after {i+1} optimisation steps.")
-    return costs
+    return costs, parameters
 
 if __name__ == "__main__":
     ru.set_global_seed(cfg.global_seed)
-    optimise_VQE()
+    costs, parameters = optimise_VQE()
+
+    m_ising, offset = qu.provide_random_maxcut_ising(cfg.n_qubits, cfg.edge_prob, cfg.problem_seed)
+    observable = cu.ising_hamiltonian(m_ising)
+
+    dev = qml.device("default.qubit")
+    circ = qml.QNode(cu.circ_2, dev, interface="tf")
+
+    f = partial(circ, observable = observable, n_layers = cfg.n_layers)
+
+    print(f(parameters) + offset)
+
+    coeffs = coefficients(f, cfg.n_layers*cfg.n_qubits*2, 2)
+    print(coeffs)
+    fig, ax = plt.subplots(2, 1, sharex=True, sharey=True, figsize=(50, 4))
+    bar(coeffs, cfg.n_layers * cfg.n_qubits * 2, ax, colour_dict={"real" : "red", "imag" : "blue"})
+    plt.savefig('coeffs.png')
+
 
