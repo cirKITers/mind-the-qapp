892bd9e94f235f051e0618acec1994c74f37c76c
lc3267 <melvin.strobl@kit.edu>
2024-03-13 10:53:56 +0100

    moved rng out of the for loops
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- app/utils/expressibility.py
+++ app/utils/expressibility.py
@@ -3,8 +3,8 @@ import numpy as np
 from typing import Tuple
 from scipy import integrate
 
-def theoretical_haar_probability(fidelity: float, n_qubits: int) \
-        -> float:
+
+def theoretical_haar_probability(fidelity: float, n_qubits: int) -> float:
     """
     Calculates theoretical probability density function for random Haar states
     as proposed by Sim et al. (https://arxiv.org/abs/1905.10876).
@@ -18,8 +18,8 @@ def theoretical_haar_probability(fidelity: float, n_qubits: int) \
     prob = (N - 1) * (1 - fidelity) ** (N - 2)
     return prob
 
-def sampled_haar_probability(n_qubits: int, n_bins: int) \
-        -> np.ndarray:
+
+def sampled_haar_probability(n_qubits: int, n_bins: int) -> np.ndarray:
     """
     Calculates theoretical probability density function for random Haar states
     as proposed by Sim et al. (https://arxiv.org/abs/1905.10876) and bins it
@@ -31,14 +31,18 @@ def sampled_haar_probability(n_qubits: int, n_bins: int) \
     """
     dist = np.zeros(n_bins)
     for i in range(n_bins):
-        l = (1/n_bins) * i
-        u = l + (1/n_bins)
-        dist[i], _ = integrate.quad(theoretical_haar_probability, l, u, args=(n_qubits,))
+        l = (1 / n_bins) * i
+        u = l + (1 / n_bins)
+        dist[i], _ = integrate.quad(
+            theoretical_haar_probability, l, u, args=(n_qubits,)
+        )
 
     return dist
 
-def get_sampled_haar_probability_histogram(n_qubits, n_bins, n_repetitions) -> \
-        Tuple[np.ndarray, np.ndarray]:
+
+def get_sampled_haar_probability_histogram(
+    n_qubits, n_bins, n_repetitions
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     Calculates theoretical probability density function for random Haar states
     as proposed by Sim et al. (https://arxiv.org/abs/1905.10876) and bins it
@@ -55,8 +59,10 @@ def get_sampled_haar_probability_histogram(n_qubits, n_bins, n_repetitions) -> \
 
     return x, y
 
+
 class Expressibility_Sampler:
-    def __init__(self,
+    def __init__(
+        self,
         n_qubits: int,
         n_layers: int,
         seed: int = 100,
@@ -87,16 +93,24 @@ class Expressibility_Sampler:
     def sample_state_fidelities(self) -> np.ndarray:
 
         fidelities = np.zeros((len(self.x_samples), self.n_samples))
+        w = (
+            2
+            * np.pi
+            * (
+                1
+                - 2
+                * self.rng.random(
+                    size=[len(self.x_samples), self.n_samples, 2, *self.model.n_params]
+                )
+            )
+        )
         for i, x in enumerate(self.x_samples):
             for s in range(self.n_samples):
 
-                w1 = 2 * np.pi * (1 - 2 * self.rng.random(size=self.model.n_params))
-                sv1 = self.model(w1, x)
+                sv1 = self.model(w[i, s, 0], x)
+                sv2 = self.model(w[i, s, 1], x)
 
-                w2 = 2 * np.pi * (1 - 2 * self.rng.random(size=self.model.n_params))
-                sv2 = self.model(w2, x)
-
-                fidelity = np.trace(np.sqrt(np.sqrt(sv1) * sv2 * np.sqrt(sv1)))**2
+                fidelity = np.trace(np.sqrt(np.sqrt(sv1) * sv2 * np.sqrt(sv1))) ** 2
                 fidelity = np.abs(fidelity)
 
                 fidelities[i, s] = fidelity
@@ -105,7 +119,7 @@ class Expressibility_Sampler:
 
     def sample_hist_state_fidelities(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         fidelities = self.sample_state_fidelities()
-        z_component = np.zeros((len(self.x_samples), self.n_bins-1))
+        z_component = np.zeros((len(self.x_samples), self.n_bins - 1))
 
         # FIXME: somehow I get nan's in the histogram, when directly creating bins until n
         # workaround hack is to add a small epsilon
@@ -114,4 +128,3 @@ class Expressibility_Sampler:
             z_component[i], _ = np.histogram(fidelities[i], bins=b, density=True)
         z_component = np.transpose(z_component)
         return self.x_samples, b, z_component
-
