2279337a41a1e13c824bd4a4e39b1fefc068fb4c
Melvin Strobl <lc3267@kit.edu>
2024-08-16 11:16:57 +0200

    started with documentation on main page
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- app/pages/0-main-page.py
+++ app/pages/0-main-page.py
@@ -1,11 +1,57 @@
 import dash
-from dash import html
+from dash import (
+    Dash,
+    dcc,
+    html,
+    Input,
+    State,
+    Output,
+    callback,
+)
 
 dash.register_page(__name__, name="Home", path="/")
 
 layout = html.Div(
     [
-        html.H1("This is our Home page"),
-        html.Div("This is our Home page content."),
+        dcc.Markdown(
+            """
+        # Mind the QApp
+        ## About
+
+        This application serves a visualization and teaching purpose for simple training scenarios to study various effects and problems in QML.
+        Characteristic paramters of a QML model can be adjusted
+        - number of qubits
+        - number of layers
+        - type of ansatz
+        - data re-uploading [1]
+
+        Furthermore as noise is a non-negligible factor in Quantum Computing, the application allows the user to adjust the strength of various types of noise.
+        This enables to study the impact of noise on the Fourier spectrum, which can be represented with the chosen ansatz [1], both with a fixed set of parameters and within a training scenario.
+
+        We can also track the entangling capability [2] of the chosen Ansatz over the training period, so that one can evaluate the effect of parameterized entangling gates on the training performance.
+
+        The entanglement and expressibility of [3] can also be analyzed in another part of the application, distinct from the training routine, where we present a visualization of expressibility with respect to the input value. 
+        This is done for randomly sampled parameter values, where the distance to the Haar measure is indicated by the Kullback-Leibler divergence, as in [3].
+
+        To improve the performance, we utilize caching strategies.
+
+        ## Getting started
+
+        On the left you can see two pages:
+        - [Training](/training)
+        - [Expressibility](/expressibility)
+
+        The first one allows you to run a training of a data-reuploading enabled model on a simple Fourier series dataset.
+        During training you can observe how the loss changes and the model output gets closer to the target.
+        Play around with the number of qubits and layers as well as the circuit type to see which model performs best.
+
+
+        ## References
+
+        1. [The effect of data encoding on the expressive power of variational quantum machine learning models](https://arxiv.org/abs/2008.08605)
+        2. [An observable measure of entanglement for pure states of multi-qubit systems](https://arxiv.org/abs/quant-ph/0305094)
+        3. [Expressibility and entangling capability of parameterized quantum circuits for hybrid quantum-classical algorithms](https://arxiv.org/abs/1905.10876)
+        """
+        ),
     ]
 )
