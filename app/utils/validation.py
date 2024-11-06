4bb7fd87403be3d4b157ed54e5988c791b826faa
Melvin Strobl <lc3267@kit.edu>
2024-11-06 10:58:04 +0100

    formatting
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- app/utils/validation.py
+++ app/utils/validation.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+
 def data_is_valid(main_data: Optional[dict], page_data: Optional[dict]) -> bool:
     """
     Checks if main data and page data exist and contain non-None values
@@ -8,6 +9,11 @@ def data_is_valid(main_data: Optional[dict], page_data: Optional[dict]) -> bool:
     :param page_data: dict, optional: Page specific data
     :return: bool: Whether data is valid
     """
-    return not (page_data is None or main_data is None or \
-            any([ p is None for p in page_data.values()] + \
-                [ p is None for p in main_data.values() ]))
+    return not (
+        page_data is None
+        or main_data is None
+        or any(
+            [p is None for p in page_data.values()]
+            + [p is None for p in main_data.values()]
+        )
+    )
